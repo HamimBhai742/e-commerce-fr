b39f2407e37cf617728e14662196ab2231239bce
HamimBhai742 <mdhamim5088@gmail.com>
2026-02-10 11:38:45 +0600

    Navbar update

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,20 @@
 const Navbar =()=>{
-    return ( <div>
-dddd
-    </div>)
+    return(
+        <nav className="flex justify-between px-10 pt-3">
+<div>
+    <h1 className=" text-amber-500 items-center font-semibold">
+        E - <span className="text-purple-500">Commerce</span>
+    </h1>
+</div>
+
+<div>
+    <ul className="flex gap-3 ">
+        <li>Home</li>
+        <li>Products</li>
+    </ul>
+</div>
+        </nav>
+    )
 }
 
 export default Navbar
